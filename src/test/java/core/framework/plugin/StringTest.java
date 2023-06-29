422aad33b1a0a348228845d71c480c8f27acd0d8
ebin <ebinsu@chancetop.com>
2023-06-29 15:16:04 +0800

    1.0.18

--- src/test/java/core/framework/plugin/StringTest.java
+++ src/test/java/core/framework/plugin/StringTest.java
@@ -7,14 +7,6 @@ import org.apache.commons.lang.StringUtils;
  */
 public class StringTest {
     public static void main(String[] args) {
-        System.out.println(StringUtils.getLevenshteinDistance("agent", "a"));
-        System.out.println(StringUtils.getLevenshteinDistance("agent", "b"));
-        System.out.println(StringUtils.getLevenshteinDistance("agent", "c"));
-        System.out.println(StringUtils.getLevenshteinDistance("agent", "abc"));
-        System.out.println(StringUtils.getLevenshteinDistance("agent", "agentEn"));
-        System.out.println(StringUtils.getLevenshteinDistance("agent", "ag"));
-        System.out.println(StringUtils.getLevenshteinDistance("agent", "agentEnum"));
-
         System.out.println(StringUtils.getLevenshteinDistance("orderId", "wonderOrderId"));
         System.out.println(StringUtils.getLevenshteinDistance("orderId", "mpOrderId"));
         System.out.println(StringUtils.getLevenshteinDistance("orderId", "OrderId"));
